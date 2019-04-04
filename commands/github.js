eca88b621fb08d88fe93351e4245ac21be204136
BentoMake <49262481+BentoMake@users.noreply.github.com>
2019-04-04 20:58:20 +0200

    Update github.js

--- commands/github.js
+++ commands/github.js
@@ -1,7 +1,7 @@
 const discord = require("discord.js")
 
 module.exports.run = (bento, message, args) => {
-    message.channel.send("Get your own BentoBot to whitelist easier on your FiveM server! Just at https://github.com/");
+    message.channel.send("Get your own BentoBot to whitelist easier on your FiveM server! Just at https://github.com/BentoMake/BentoBot");
 }
 
 module.exports.help = {
